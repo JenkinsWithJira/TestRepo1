3c8fc803df8d2d48ca9a8602f498ffcc0a5868a6
VijayJha <vijay.jha@aapc.com>
2016-10-20 15:03:10 +0530

    Very First Commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowMessage.Show();
+
+            Console.ReadLine();
+        }
+    }
+
+    class ShowMessage
+    {
+        public static void Show()
+        {
+            Console.WriteLine("Hello World.");
+        }
+
+    }
+}
